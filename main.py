58732b3dfcbec6ad438b6d76f9d4a8b9164335d4
32BitFlame <47258322+32BitFlame@users.noreply.github.com>
2020-08-23 21:07:11 -0400

    Started testing with kivy

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import kivy
 from kivy.app import App
 
 from kivy.core.window import Window
-
+from kivy.uix.tabbedpanel import TabbedPanel
 Window.size = (600,600)
+
+class WindowTabbedPanel(TabbedPanel):
+    pass
 
 class PySoundboardApp(App):
     def build(FloatLayout):
         pass
+
+PySoundboardApp().run()
